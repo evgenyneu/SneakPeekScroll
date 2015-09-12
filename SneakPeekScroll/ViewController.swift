254088556481252ec915204e673a7770184e538d
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-09-13 08:36:35 +1000

    Initial commit

---
+++ SneakPeekScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SneakPeekScroll
+//
+//  Created by Evgenii on 13/09/2015.
+//  Copyright © 2015 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
