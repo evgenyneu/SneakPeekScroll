4061c9ce6a90a0c7aa5c5b4d3447ce12c773bb14
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-09-13 09:15:07 +1000

    Add subviews

--- SneakPeekScroll/ViewController.swift
+++ SneakPeekScroll/ViewController.swift
@@ -1,25 +1,66 @@
-//
-//  ViewController.swift
-//  SneakPeekScroll
-//
-//  Created by Evgenii on 13/09/2015.
-//  Copyright © 2015 Evgenii Neumerzhitckii. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var scrollView: UIScrollView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    addViews()
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  /// Add all the subviews to the scroll view
+  private func addViews() {
+    let subviews = (1...10).map { _ in addSingleView() }
+    
+    // Add constraints between the subviews so they are shown one after another
+    iiAutolayoutConstraints.viewsNextToEachOther(subviews, constraintContainer: scrollView)
+    
+    // Align the leading edge of the first subview with the leading edge of the scroll view
+    if let firstSubview = subviews.first {
+      iiAutolayoutConstraints.alignSameAttributes(firstSubview, toItem: scrollView,
+        constraintContainer: scrollView, attribute: NSLayoutAttribute.Leading)
+    }
+    
+    // Align the trailing edge of the first subview with the trailing edge of the scroll view
+    if let lastSubview = subviews.last {
+      iiAutolayoutConstraints.alignSameAttributes(lastSubview, toItem: scrollView,
+        constraintContainer: scrollView, attribute: NSLayoutAttribute.Trailing)
+    }
+    
+    // Style the subview
+    styleSubviews(subviews)
   }
-
-
+  
+  /// Add a subview to the scroll view
+  private func addSingleView() -> UIView {
+    let subview = UIView()
+    subview.translatesAutoresizingMaskIntoConstraints = false
+    scrollView.addSubview(subview)
+    
+    // Make the width of the subview equal to 80% of the scroll view width
+    iiAutolayoutConstraints.equalWidth(subview, viewTwo: scrollView,
+      constraintContainer: scrollView, multiplier: 0.8)
+    
+    // Make heiht of the subview the same as the scroll view
+    iiAutolayoutConstraints.fillParent(subview, parentView: scrollView, margin: 0,
+      vertically: true)
+    
+    // Center the subview vertically in the scroll view
+    iiAutolayoutConstraints.alignSameAttributes(subview, toItem: scrollView,
+      constraintContainer: scrollView, attribute: NSLayoutAttribute.CenterY)
+    
+    return subview
+  }
+  
+  private func styleSubviews(views: [UIView]) {
+    for view in views {
+      style(view)
+    }
+  }
+  
+  private func style(view: UIView) {
+    view.backgroundColor = UIColor.redColor()
+  }
 }
 
---
+++ SneakPeekScroll/iiAutoLayout.swift
@@ -0,0 +1,183 @@
+//
+//  Collection of shortcuts to create autolayout constraints.
+//
+
+import UIKit
+
+class iiAutolayoutConstraints {
+  class func fillParent(view: UIView, parentView: UIView, margin: CGFloat = 0, vertically: Bool = false) {
+    var marginFormat = ""
+
+    if margin != 0 {
+      marginFormat = "-(\(margin))-"
+    }
+
+    var format = "|\(marginFormat)[view]\(marginFormat)|"
+
+    if vertically {
+      format = "V:" + format
+    }
+
+    let constraints = NSLayoutConstraint.constraintsWithVisualFormat(format,
+      options: [], metrics: nil,
+      views: ["view": view])
+
+    parentView.addConstraints(constraints)
+  }
+
+  // MARK: - Align view next to each other
+
+  class func viewsNextToEachOther(views: [UIView],
+    constraintContainer: UIView, margin: CGFloat = 0,
+    vertically: Bool = false) -> [NSLayoutConstraint] {
+
+    if views.count < 2 { return []  }
+
+    var constraints = [NSLayoutConstraint]()
+
+    for (index, view) in views.enumerate() {
+      if index >= views.count - 1 { break }
+
+      let viewTwo = views[index + 1]
+
+      constraints += twoViewsNextToEachOther(view, viewTwo: viewTwo,
+        constraintContainer: constraintContainer, margin: margin, vertically: vertically)
+    }
+
+    return constraints
+  }
+
+  class func twoViewsNextToEachOther(viewOne: UIView, viewTwo: UIView,
+    constraintContainer: UIView, margin: CGFloat = 0,
+    vertically: Bool = false) -> [NSLayoutConstraint] {
+
+    var marginFormat = ""
+
+    if margin != 0 {
+      marginFormat = "-\(margin)-"
+    }
+
+    var format = "[viewOne]\(marginFormat)[viewTwo]"
+
+    if vertically {
+      format = "V:" + format
+    }
+
+    let constraints = NSLayoutConstraint.constraintsWithVisualFormat(format,
+      options: [], metrics: nil,
+      views: [ "viewOne": viewOne, "viewTwo": viewTwo ])
+
+    constraintContainer.addConstraints(constraints)
+
+    return constraints
+  }
+  
+  class func equalWidth(viewOne: UIView, viewTwo: UIView, constraintContainer: UIView,
+    multiplier: CGFloat = 1)  -> [NSLayoutConstraint] {
+    
+    return equalWidthOrHeight(viewOne, viewTwo: viewTwo,
+      constraintContainer: constraintContainer, isHeight: false)
+  }
+  
+  private class func equalWidthOrHeight(viewOne: UIView, viewTwo: UIView, constraintContainer: UIView,
+    isHeight: Bool, multiplier: CGFloat = 1) -> [NSLayoutConstraint] {
+      
+    let layoutAttribute = isHeight ? NSLayoutAttribute.Height : NSLayoutAttribute.Width
+    
+    let constraint = NSLayoutConstraint(
+      item: viewOne,
+      attribute: layoutAttribute,
+      relatedBy: NSLayoutRelation.Equal,
+      toItem: viewTwo,
+      attribute: layoutAttribute,
+      multiplier: multiplier,
+      constant: 0)
+    
+    let constraints = [constraint]
+    NSLayoutConstraint.activateConstraints(constraints)
+    
+    return constraints
+  }
+  
+  class func alignSameAttributes(item: AnyObject, toItem: AnyObject,
+    constraintContainer: UIView, attribute: NSLayoutAttribute, margin: CGFloat = 0) -> [NSLayoutConstraint] {
+
+    let constraint = NSLayoutConstraint(
+      item: item,
+      attribute: attribute,
+      relatedBy: NSLayoutRelation.Equal,
+      toItem: toItem,
+      attribute: attribute,
+      multiplier: 1,
+      constant: margin)
+
+    constraintContainer.addConstraint(constraint)
+
+    return [constraint]
+  }
+  
+    //
+    //  class func equalWidth(viewOne: UIView, viewTwo: UIView, constraintContainer: UIView) -> [NSLayoutConstraint] {
+    //
+    //    return equalWidthOrHeight(viewOne, viewTwo: viewTwo, constraintContainer: constraintContainer, isHeight: false)
+    //  }
+    //
+    //  // MARK: - Equal height and width
+    //
+    //  class func equalHeight(viewOne: UIView, viewTwo: UIView, constraintContainer: UIView) -> [NSLayoutConstraint] {
+    //
+    //    return equalWidthOrHeight(viewOne, viewTwo: viewTwo, constraintContainer: constraintContainer, isHeight: true)
+    //  }
+    //
+    //  class func equalSize(viewOne: UIView, viewTwo: UIView, constraintContainer: UIView) -> [NSLayoutConstraint] {
+    //
+    //    var constraints = equalWidthOrHeight(viewOne, viewTwo: viewTwo, constraintContainer: constraintContainer, isHeight: false)
+    //
+    //    constraints += equalWidthOrHeight(viewOne, viewTwo: viewTwo, constraintContainer: constraintContainer, isHeight: true)
+    //
+    //    return constraints
+    //  }
+    //
+    //  class func equalWidthOrHeight(viewOne: UIView, viewTwo: UIView, constraintContainer: UIView,
+    //    isHeight: Bool) -> [NSLayoutConstraint] {
+    //
+    //    var prefix = ""
+    //
+    //    if isHeight { prefix = "V:" }
+    //
+    //    let constraints = NSLayoutConstraint.constraintsWithVisualFormat("\(prefix)[viewOne(==viewTwo)]",
+    //      options: [], metrics: nil,
+    //      views: ["viewOne": viewOne, "viewTwo": viewTwo])
+    //
+    //    constraintContainer.addConstraints(constraints)
+    //
+    //    return constraints
+    //  }
+    //
+    //
+    //  class func height(view: UIView, value: CGFloat) -> [NSLayoutConstraint] {
+    //    return widthOrHeight(view, value: value, isHeight: true)
+    //  }
+    //
+    //  class func width(view: UIView, value: CGFloat) -> [NSLayoutConstraint] {
+    //    return widthOrHeight(view, value: value, isHeight: false)
+    //  }
+    //
+    //  class func widthOrHeight(view: UIView, value: CGFloat, isHeight: Bool) -> [NSLayoutConstraint] {
+    //
+    //    let layoutAttribute = isHeight ? NSLayoutAttribute.Height : NSLayoutAttribute.Width
+    //
+    //    let constraint = NSLayoutConstraint(
+    //      item: view,
+    //      attribute: layoutAttribute,
+    //      relatedBy: NSLayoutRelation.Equal,
+    //      toItem: nil,
+    //      attribute: NSLayoutAttribute.NotAnAttribute,
+    //      multiplier: 1,
+    //      constant: value)
+    //
+    //    view.addConstraint(constraint)
+    //
+    //    return [constraint]
+    //  }
+}
